f0ea41774cfc104d910d8a8619c3d4b843955f4d
CodingNinja <CodingNinja@outlook.com>
2025-11-06 10:21:40 +0800

    添加项目文件。

---
+++ ListBoxItemWidthIssue/MainWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows;
+
+namespace ListBoxItemWidthIssue;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public ObservableCollection<string> Data { get; } = [];
+
+    public MainWindow()
+    {
+        InitializeComponent();
+
+        DataContext = this;
+
+        for (int i = 0; i < 1000; i++)
+        {
+            string item = "Item " + new string('*', Random.Shared.Next(1, 50));
+
+            Data.Add(item);
+        }
+    }
+}
